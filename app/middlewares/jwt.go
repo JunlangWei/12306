1fd9eaf87e14c512d342c4f204555a4c91293c53
mamachengcheng <616254086@qq.com>
2020-11-21 13:41:50 +0800

    fix jwt

--- app/middlewares/jwt.go
+++ app/middlewares/jwt.go
@@ -1,52 +1,50 @@
 package middlewares
 
 import (
-	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	"net/http"
-	"time"
 )
 
-var jwtSecret = []byte()
-
-type Claims struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
-	jwt.StandardClaims
-}
-
-func GenerateToken(username, password string) (string, error) {
-	nowTime := time.Now()
-	expireTime := nowTime.Add(12 * time.Hour)
-
-	claims := Claims{
-		username,
-		password,
-		jwt.StandardClaims{
-			ExpiresAt: expireTime.Unix(),
-			Issuer:    "12306",
-		},
-	}
-
-	tokenClaims := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	token, err := tokenClaims.SignedString(jwtSecret)
-
-	return token, err
-}
-
-func ParseToken(token string) (*Claims, error) {
-	tokenClaims, err := jwt.ParseWithClaims(token, &Claims{}, func(token *jwt.Token) (interface{}, error) {
-		return jwtSecret, nil
-	})
-
-	if tokenClaims != nil {
-		if claims, ok := tokenClaims.Claims; ok && tokenClaims.Valid {
-			return claims, nil
-		}
-	}
-
-	return nil, err
-}
+//var jwtSecret = []byte()
+//
+//type Claims struct {
+//	Username string `json:"username"`
+//	Password string `json:"password"`
+//	jwt.StandardClaims
+//}
+//
+//func GenerateToken(username, password string) (string, error) {
+//	nowTime := time.Now()
+//	expireTime := nowTime.Add(12 * time.Hour)
+//
+//	claims := Claims{
+//		username,
+//		password,
+//		jwt.StandardClaims{
+//			ExpiresAt: expireTime.Unix(),
+//			Issuer:    "12306",
+//		},
+//	}
+//
+//	tokenClaims := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+//	token, err := tokenClaims.SignedString(jwtSecret)
+//
+//	return token, err
+//}
+//
+//func ParseToken(token string) (*Claims, error) {
+//	tokenClaims, err := jwt.ParseWithClaims(token, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+//		return jwtSecret, nil
+//	})
+//
+//	if tokenClaims != nil {
+//		if claims, ok := tokenClaims.Claims; ok && tokenClaims.Valid {
+//			return claims, nil
+//		}
+//	}
+//
+//	return nil, err
+//}
 
 func jwtAbort(c *gin.Context, msg string) {
 	c.JSON(http.StatusUnauthorized, gin.H{
